c3f077e33a78326f2a1561cdaff65650920f0ccc
kagHarsh <harshkagh@gmail.com>
2024-09-10 12:35:17 +0530

    msg

--- Team5.py
+++ Team5.py
@@ -1,7 +1,8 @@
 import random
 
-random_numbers = [random.randint(1, 100) for _ in range(10)]
-print(random_numbers)
+random_numbers_is_not = [random.randint(1, 100) for _ in range(10)]
+print(random_numbers_is_not)
+
 
 #add numbers
 def calculate_sum(numbers):
